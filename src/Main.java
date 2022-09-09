b08f6c2f17bbb3a9e3be361d54424b434d85fc8a
cihadozdemir <ciha0276@stud.kea.dk>
2022-09-09 12:56:05 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Hej");
+        System.out.println("Verden");
+        System.out.println("har i det godt");
+    }
+}
